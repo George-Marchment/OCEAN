4be08c8f3a988fee20a1216bb897d0d37b454185
jeromecst <jerome.coqui@gmail.com>
2020-04-18 01:20:29 +0200

    everything looks right

--- starting_kit/code/testModel.py
+++ starting_kit/code/testModel.py
@@ -47,62 +47,64 @@ a.fit(X_train, Y_train)
 aP = a.predict(X_test)
 metric_name, scoring_function = get_metric()
 print('Using scoring metric:', metric_name)
-#sc = make_scorer(scoring_function)
-#a.printScore(sc,aP, Y_train)
 
-#"""
-#preprocessing fatas
-#"""
-#pre = Preprocessor()
-#X_train, Y_train = pre.fit_transform(X_train, Y_train)
-#X_test = pre.transform(X_test)
-
-#print(X_train.shape, " ", Y_train.shape, " ", X_test.shape)
-
-#model_list = [
-#    Perceptron(),
-#    GaussianNB(),
-#    KNeighborsClassifier(),
-#    DecisionTreeClassifier(),
-#    RandomForestClassifier(),
-#    AdaBoostClassifier(),
-#    GradientBoostingClassifier()
-#]
-
-#param_list = [
-#    {'tol': [1e-1, 1e-3, 1e-5], 'eta0': [0.01, 0.1, 1, 10]},
-#    {'var_smoothing': [1e-1, 1e-5, 1e-9, 1e-14]},
-#    {'n_neighbors': [1, 2, 3, 5, 10]},
-#    {'max_depth': [10, 100, 500, 1000], 'max_features': ('sqrt', 'auto', 'log2')},
-#    {'n_estimators': [290, 300, 310, 320, 330, 340], 'warm_start': (True, False), 'bootstrap': (True, False)},
-#    {'n_estimators': [10, 50, 100, 150], 'learning_rate': [0.5, 0.75, 1, 1.25, 1.5]},
-#    {'learning_rate': [0.05, 0.1, 0.2], 'n_estimators': [50, 100, 150, 200], 'warm_start': (True, False)}
-#]
-
-#"""
-#Trouver le meilleur clasif __name__ ==sifieur avec les meilleurs paramètres
-#"""
-#clf = BestClf(model_list, param_list, X_train, Y_train)
-#clf.train()
-
-#print("meilleurs param = ", clf.bestParam)
-#"""
-#Le meilleur modèle est initialisé et on teste son score
-#"""
-
-#M = model(clf.bestClf, clf.bestParam)
-#M.fit(X_train, Y_train)
-#m_train = M.predict(X_train)
+print(scoring_function(Y_test, aP))
 
 
+# sc = make_scorer(scoring_function)
+# a.printScore(sc,aP, Y_train)
 
-#print('Training score for the', metric_name, 'metric = %5.4f' % scoring_function(Y_test, m_train))
-#scoresM = cross_val_score(M, X_train, Y_train.ravel(), cv=5, scoring=make_scorer(scoring_function), n_jobs=-1)
-#print()
+# """
+# preprocessing fatas
+# """
+# pre = Preprocessor()
+# X_train, Y_train = pre.fit_transform(X_train, Y_train)
+# X_test = pre.transform(X_test)
 
-#"""
-#Tests unitaires
-#"""
+# print(X_train.shape, " ", Y_train.shape, " ", X_test.shape)
 
-#T = test(M.clf, M.param, X_train, Y_train, X_test, Y_test, scoring_function)
-#T.allTests()
+# model_list = [
+#     Perceptron(),
+#     GaussianNB(),
+#     KNeighborsClassifier(),
+#     DecisionTreeClassifier(),
+#     RandomForestClassifier(),
+#     AdaBoostClassifier(),
+#     GradientBoostingClassifier()
+# ]
+
+# param_list = [
+#     {'tol': [1e-1, 1e-3, 1e-5], 'eta0': [0.01, 0.1, 1, 10]},
+#     {'var_smoothing': [1e-1, 1e-5, 1e-9, 1e-14]},
+#     {'n_neighbors': [1, 2, 3, 5, 10]},
+#     {'max_depth': [10, 100, 500, 1000], 'max_features': ('sqrt', 'auto', 'log2')},
+#     {'n_estimators': [290, 300, 310, 320, 330, 340], 'warm_start': (True, False), 'bootstrap': (True, False)},
+#     {'n_estimators': [10, 50, 100, 150], 'learning_rate': [0.5, 0.75, 1, 1.25, 1.5]},
+#     {'learning_rate': [0.05, 0.1, 0.2], 'n_estimators': [50, 100, 150, 200], 'warm_start': (True, False)}
+# ]
+
+# """
+# Trouver le meilleur clasif __name__ ==sifieur avec les meilleurs paramètres
+# """
+# clf = BestClf(model_list, param_list, X_train, Y_train)
+# clf.train()
+
+# print("meilleurs param = ", clf.bestParam)
+# """
+# Le meilleur modèle est initialisé et on teste son score
+# """
+
+# M = model(clf.bestClf, clf.bestParam)
+# M.fit(X_train, Y_train)
+# m_train = M.predict(X_train)
+
+# print('Training score for the', metric_name, 'metric = %5.4f' % scoring_function(Y_test, m_train))
+# scoresM = cross_val_score(M, X_train, Y_train.ravel(), cv=5, scoring=make_scorer(scoring_function), n_jobs=-1)
+# print()
+
+# """
+# Tests unitaires
+# """
+
+# T = test(M.clf, M.param, X_train, Y_train, X_test, Y_test, scoring_function)
+# T.allTests()
